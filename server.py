f6abd814a4a7824346b9b36aab138e169a3ba3ee
Nyaundi Brian <ndieksman@gmail.com>
2015-11-02 18:49:01 +0300

    create python server

---
+++ server.py
@@ -0,0 +1,30 @@
+# Simple server
+__author__ = 'brian'
+import socket
+
+#run on localhost at port 3000
+HOST, PORT = '', 3000
+
+#create socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+if __name__ == '__main__':
+    server.bind((HOST, PORT))
+    server.listen(5)
+
+    print('Server running on port  %s ...' % PORT)
+
+    #Server Loop
+    while True:
+        connection, connection_address = server.accept()
+        request = connection.recv(1024)
+
+
+        print("From ", connection_address, request.decode('utf-8').split('\n')[0],end='\n')
+        #send a response
+        connection.sendall('hello world from python server'.encode())
+        connection.close()
+        connection=None
+
