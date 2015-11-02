de50314482e316aa002e8e23178838b8585c1ed4
Nyaundi Brian <ndieksman@gmail.com>
2015-11-02 19:11:38 +0300

    rename file and class to Server

--- Server.java
+++ Server.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Server {
 
     public static void main(String[] args) {
         ServerSocket serverSocket=null;
